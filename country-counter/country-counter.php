b0f2d93cf95bb5d887c3a17bc542e2ba5f65bb33
David Beauchamp <david123beauchamp@gmail.com>
2018-07-18 14:24:03 +0200

    Starting to build the bar graph feature

--- country-counter/country-counter.php
+++ country-counter/country-counter.php
@@ -13,7 +13,7 @@ class country_counter_widget extends WP_Widget {
 	// Create Multiple WordPress Widgets
   function __construct() {
     parent::__construct('country_counter_widget', __('Country Counter', 'country_counter_plugin_domain'), array(
-      'description' => __('Display the percentage and graph of countries been to.', 'country_counter_plugin_domain')
+      'classname' => 'ccw-widget', 'description' => __('Display the percentage and graph of countries been to.', 'country_counter_plugin_domain')
     ));
   }
     
@@ -29,141 +29,237 @@ class country_counter_widget extends WP_Widget {
     $ccw_oceania = apply_filters('ccw_oceania', $instance['ccw_oceania']);
   
     $ccw_world_group = '
-  <div class="ccw-container">
-    <div class="charts-container">
-      <p class="ccw-intro"><span id="ccw-intro-num"></span> Countries Visited</p>
-      <div class="pie-wrapper">
-        <span class="ccw-label"><span id="ccw-num-percent">45</span><span class="ccw-smaller">%</span></span>
-        <div id="ccw-pie" class="ccw-pie">
-          <div id="ccw-left-side" class="ccw-left-side ccw-half-circle"></div>
-          <div class="ccw-right-side ccw-half-circle"></div>
+      <div class="ccw-container">
+        <div class="charts-container">
+          <p class="ccw-intro"><span id="ccw-intro-num"></span> Countries Visited</p>
+          <div class="pie-wrapper">
+            <span class="ccw-label"><span id="ccw-num-percent"></span><span class="ccw-smaller">%</span></span>
+            <div id="ccw-pie" class="ccw-pie">
+              <div id="ccw-left-side" class="ccw-left-side ccw-half-circle"></div>
+              <div class="ccw-right-side ccw-half-circle"></div>
+            </div>
+            <div class="ccw-shadow"></div>
+          </div>
         </div>
-        <div class="ccw-shadow"></div>
+
+        <ul class="ccw-bar-graph">
+          <li id="ccw-na" class="ccw-na"></li>
+          <li id="ccw-sa" class="ccw-sa"></li>
+          <li id="ccw-eu" class="ccw-eu"></li>
+          <li id="ccw-af" class="ccw-af"></li>
+          <li id="ccw-as" class="ccw-as"></li>
+          <li id="ccw-oc" class="ccw-oc"></li>
+        </ul>
+
       </div>
-    </div>
-  </div>
-  
-  <style>
-  .ccw-container *,
-  .ccw-container *:before,
-  .ccw-container *:after {
-    box-sizing: border-box;
-  }
-  .ccw-container {
-    color: #444;
-    font-family: "Lato", Tahoma, Geneva, sans-serif;
-    font-size: 16px;
-    padding: 10px;
-  }
-  .ccw-container .charts-container {
-    font-size: 160px;
-  }
-  .ccw-intro {
-    font-size: 18px;
-    margin-bottom: 15px;
-    text-align: center;
-    color: #7f8c8d;
-    font-weight: 600;
-  }
-  .ccw-container .charts-container:after {
-    clear: both;
-    content: "";
-    display: table;
-  }
-  .ccw-container .pie-wrapper {
-    height: 1em;
-    width: 1em;
-    margin-left: auto;
-    margin-right: auto;
-    position: relative;
-  }
-  .ccw-container .ccw-pie {
-    height: 100%;
-    width: 100%;
-    clip: rect(0, 1em, 1em, 0.5em);
-    left: 0;
-    position: absolute;
-    top: 0;
-  }
-  .ccw-container .ccw-pie .ccw-half-circle {
-    height: 100%;
-    width: 100%;
-    border: 0.1em solid #3498db;
-    border-color: #1abc9c;
-    border-radius: 50%;
-    clip: rect(0, 0.5em, 1em, 0);
-    left: 0;
-    position: absolute;
-    top: 0;
-  }
-  .ccw-container .ccw-label {
-    background: none;
-    border-radius: 50%;
-    bottom: 0.4em;
-    color: #7f8c8d;
-    cursor: default;
-    display: block;
-    font-size: 0.25em;
-    left: 0.4em;
-    line-height: 2.6em;
-    position: absolute;
-    right: 0.4em;
-    text-align: center;
-    top: 0.4em;
-  }
-  .ccw-container .ccw-label .ccw-smaller {
-    color: #bdc3c7;
-    font-size: .45em;
-    padding-bottom: 20px;
-    vertical-align: super;
-  }
-  .ccw-container .ccw-shadow {
-    height: 100%;
-    width: 100%;
-    border: 0.1em solid #bdc3c7;
-    border-radius: 50%;
-  }
-  .pie-wrapper .ccw-pie .ccw-right-side {
-    display: none;
-    -webkit-transform: rotate(180deg);
-    transform: rotate(180deg);
-  }
-  /* toggle class .ccw-above-180 if above 180 */
-  .pie-wrapper .ccw-above-180 {
-    clip: rect(auto, auto, auto, auto);
-  }
-  /* This needs to stay at 180 */
-  .pie-wrapper .ccw-pie.ccw-above-180 .ccw-right-side {
-    display: block;
-  }
-  </style>
-  
-  <script>
-    var ccwTotal = ' . $ccw_world . ';
+      
+      <style>
+      /* bar graph */
+      .ccw-bar-graph {
+        list-style: none;
+        padding-top: 20px;
+        width:560px;
+      }
+      
+      .ccw-bar-graph li {
+        height: 35px;
+        width: 50%;
+        color: white;
+        text-align: left;
+        font-style: italic;
+        font-weight:bolder;
+        font-size: 14px;
+        line-height: 35px;
+        padding: 0;
+        margin-bottom: 5px;
+      }
+      
+      .ccw-bar-graph li span {
+        position: absolute;
+        color: #7f8c8d;
+        padding: 0px 20px;
+      }
+      .ccw-bar-graph .ccw-na {
+        background: rgba(85, 239, 196,.4);
+      }
+      .ccw-bar-graph .ccw-sa {
+        background: rgba(116, 185, 255,.4);
+      }
+      .ccw-bar-graph .ccw-eu {
+        background: rgba(162, 155, 254,.4);
+      }
+      .ccw-bar-graph .ccw-af {
+        background: rgba(253, 203, 110,.4);
+      }
+      .ccw-bar-graph .ccw-as {
+        background: rgba(255, 118, 117,.4);
+      }
+      .ccw-bar-graph .ccw-oc {
+        background: rgba(178, 190, 195,.4);
+      }
+      
+      
+      .ccw-container *,
+      .ccw-container *:before,
+      .ccw-container *:after {
+        box-sizing: border-box;
+      }
+      .ccw-widget {
+      
+      }
+      .ccw-container {
+        color: #444;
+        font-family: "Lato", Tahoma, Geneva, sans-serif;
+        font-size: 16px;
+        padding: 10px;
+      }
+      .ccw-container .charts-container {
+        font-size: 160px;
+      }
+      .ccw-intro {
+        font-size: 18px;
+        margin-bottom: 15px;
+        text-align: center;
+        color: #7f8c8d;
+        font-weight: 600;
+      }
+      .ccw-container .charts-container:after {
+        clear: both;
+        content: "";
+        display: table;
+      }
+      .ccw-container .pie-wrapper {
+        height: 1em;
+        width: 1em;
+        margin-left: auto;
+        margin-right: auto;
+        position: relative;
+      }
+      .ccw-container .ccw-pie {
+        height: 100%;
+        width: 100%;
+        clip: rect(0, 1em, 1em, 0.5em);
+        left: 0;
+        position: absolute;
+        top: 0;
+      }
+      .ccw-container .ccw-pie .ccw-half-circle {
+        height: 100%;
+        width: 100%;
+        border: 0.1em solid #3498db;
+        border-color: #1abc9c;
+        border-radius: 50%;
+        clip: rect(0, 0.5em, 1em, 0);
+        left: 0;
+        position: absolute;
+        top: 0;
+      }
+      .ccw-container .ccw-label {
+        background: none;
+        border-radius: 50%;
+        bottom: 0.4em;
+        color: #7f8c8d;
+        cursor: default;
+        display: block;
+        font-size: 0.25em;
+        left: 0.4em;
+        line-height: 2.6em;
+        position: absolute;
+        right: 0.4em;
+        text-align: center;
+        top: 0.4em;
+      }
+      .ccw-container .ccw-label .ccw-smaller {
+        color: #bdc3c7;
+        font-size: .45em;
+        padding-bottom: 20px;
+        vertical-align: super;
+      }
+      .ccw-container .ccw-shadow {
+        height: 100%;
+        width: 100%;
+        border: 0.1em solid #bdc3c7;
+        border-radius: 50%;
+      }
+      .pie-wrapper .ccw-pie .ccw-right-side {
+        display: none;
+        -webkit-transform: rotate(180deg);
+        transform: rotate(180deg);
+      }
+      /* toggle class .ccw-above-180 if above 180 */
+      .pie-wrapper .ccw-above-180 {
+        clip: rect(auto, auto, auto, auto);
+      }
+      /* This needs to stay at 180 */
+      .pie-wrapper .ccw-pie.ccw-above-180 .ccw-right-side {
+        display: block;
+      }
+      </style>
+      
+      <script>
+        var ccwTotal = ' . $ccw_world . ';
+        
+        if(ccwTotal) {
     
-    document.getElementById("ccw-intro-num").innerHTML = (ccwTotal).toString();
-    var ccwPercent100 = ccwTotal/194;
-    console.log(ccwPercent100);
-    var ccwPercent100 = Math.round(ccwPercent100 * 100) / 100;
-    console.log(ccwPercent100);
-  
-    document.getElementById("ccw-num-percent").innerHTML = (ccwPercent100 * 100).toString();
-  
-    var ccwPercent360 = 360 * ccwPercent100;
-    console.log(ccwPercent360);
-    var ccwPercent360 = Math.round(ccwPercent360);
-    console.log(ccwPercent360);
-    var ccwPercent360 = ccwPercent360.toString();
-  
-    var ccwOver180Left = document.getElementById("ccw-left-side");
-    ccwOver180Left.setAttribute("style", "-webkit-transform: rotate(" + ccwPercent360 + "deg);transform: rotate(" + ccwPercent360 + "deg);");
-    if(ccwPercent360 > 180) {
-      var ccwOver180Pie = document.getElementById("ccw-pie");
-      ccwOver180Pie.classList.add("ccw-above-180");
-    }
-  </script>
-    
-    
+          document.getElementById("ccw-intro-num").innerHTML = (ccwTotal).toString();
+          var ccwPercent100 = ccwTotal/194;
+          var ccwPercent100 =  Math.round(ccwPercent100 * 100) / 100;
+          document.getElementById("ccw-num-percent").innerHTML = (ccwPercent100 * 100).toString();
+          var ccwPercent360 = 360 * ccwPercent100;
+          var ccwPercent360 = Math.round(ccwPercent360);
+          var ccwPercent360 = ccwPercent360.toString();
+          var ccwOver180Left = document.getElementById("ccw-left-side");
+          ccwOver180Left.setAttribute("style", "-webkit-transform: rotate(" + ccwPercent360 + "deg);transform: rotate(" + ccwPercent360 + "deg);");
+          if(ccwPercent360 > 180) {
+            var ccwOver180Pie = document.getElementById("ccw-pie");
+            ccwOver180Pie.classList.add("ccw-above-180");
+          }
+        }
+      
+        if(ccwTotal) {
+          var ccwNa = 21;
+          var ccwNaEl = document.getElementById("ccw-na");
+          ccwNaEl.innerHTML = "<span>North America " + ccwNa.toString() + "/23</span>";
+          ccwNa = Math.round((ccwNa/23) * 100);
+          ccwNaEl.setAttribute("style", "width:" + ccwNa + "%;");
+          console.log(ccwNa);
+      
+          var ccwSa = 2;
+          var ccwSaEl = document.getElementById("ccw-sa");
+          ccwSaEl.innerHTML = "<span>South America " + ccwSa.toString() + "/12</span>";
+          ccwSa = Math.round((ccwSa/12) * 100);
+          ccwSaEl.setAttribute("style", "width:" + ccwSa + "%;");
+          console.log(ccwSa);
+      
+          var ccwEu = 22;
+          var ccwEuEl = document.getElementById("ccw-eu");
+          ccwEuEl.innerHTML = "<span>Europe " + ccwEu.toString() + "/47</span>";
+          ccwEu = Math.round((ccwEu/47) * 100);
+          ccwEuEl.setAttribute("style", "width:" + ccwEu + "%;");
+          console.log(ccwEu);
+      
+          var ccwAf = 1;
+          var ccwAfEl = document.getElementById("ccw-af");
+          ccwAfEl.innerHTML = "<span>Africa " + ccwAf.toString() + "/54</span>";
+          ccwAf = Math.round((ccwAf/54) * 100);
+          ccwAfEl.setAttribute("style", "width:" + ccwAf + "%;");
+          console.log(ccwAf);
+      
+          var ccwAs = 11;
+          var ccwAsEl = document.getElementById("ccw-as");
+          ccwAsEl.innerHTML = "<span>Asia " + ccwAs.toString() + "/44</span>";
+          ccwAs = Math.round((ccwAs/44) * 100);
+          ccwAsEl.setAttribute("style", "width:" + ccwAs + "%;");
+          console.log(ccwAs);
+      
+          var ccwOc = 5;
+          var ccwOcEl = document.getElementById("ccw-oc");
+          ccwOcEl.innerHTML = "<span>Oceania " + ccwOc.toString() + "/14</span>";
+          ccwOc = Math.round((ccwOc/14) * 100);
+          ccwOcEl.setAttribute("style", "width:" + ccwOc + "%;");
+          console.log(ccwOc);
+        }
+      </script>
     ';
     $ccw_continents_group = 'blah';
     
